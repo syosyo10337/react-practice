43bbe47853a54d236756e18ef4d9861069daeff2
syosyo10337 <syosyo.ds@gmail.com>
2022-12-26 21:47:51 +0900

    add useState

--- todo_web/src/App.js
+++ todo_web/src/App.js
@@ -1,26 +1,64 @@
+import { Center, Box, CheckboxGroup, Text } from "@chakra-ui/react";
+import { useState } from "react";
+
 import logo from './logo.svg';
 import './App.css';
 import Task from "./components/Task";
 
-const tasks = [
-  { id: 1, content: "買い物"},
-  { id: 2, content: "ランニング"},
-  { id: 3, content: "プログラミング学習"},
+
+const initialTasks = [
+  { 
+    id: 1, 
+    content: "買い物", 
+    isDone: false,
+  },
+  { 
+    id: 2, 
+    content: "ランニング", 
+    isDone: false,
+  },
+  { 
+    id: 3, 
+    content: "プログラミング学習", 
+    isDone: false,
+  },
 ]
 function App() {
+  const [tasks, setTasks] = useState(initialTasks);
+
+  const toggleIsDone = (index) => {
+    const tasksCopy = [ ...tasks];
+    const newIsDone = tasksCopy[index].isDone;
+    tasksCopy[index].isDone = !newIsDone;
+    setTasks(tasksCopy);
+
+  }
+
   return (
-    <> 
-      <h1>タスク一覧</h1>
-      <div>
-        {tasks.map(task => {
-          return (
-            <div key={task.id}>
-              <Task name={task.content}/>     
-            </div>
-          )
-        })}
-      </div>
-    </>
+    <Box mt="64px">
+      <Center>
+        <Box>
+          <Box mb="24px">
+            <Text fontSize="24px" as="b">
+              タスク一覧
+            </Text>
+          </Box>
+          <CheckboxGroup>
+            {tasks.map((task, index)=> {
+              return (
+                <Task 
+                key={task.id}
+                index={index}
+                name={task.content}
+                isDone={task.isDone}
+                toggleIsDone={toggleIsDone}
+                />
+                )
+              })}
+          </CheckboxGroup>
+        </Box>
+      </Center>
+    </Box>
   );
 }
 
--- todo_web/src/components/Task.jsx
+++ todo_web/src/components/Task.jsx
@@ -1,6 +1,17 @@
-const Task = ({name}) => {
+import { Checkbox, Box, Text } from "@chakra-ui/react";
+
+const Task = ({ index, name, isDone, toggleIsDone}) => {
   return (
-    <li>{name}</li>
+    <Box mb="16px">
+      <Checkbox 
+      colorScheme="blue" 
+      size="lg" 
+      isChecked={isDone}
+      onChange={() => toggleIsDone(index)}>
+        <Text>{name}</Text>
+      </Checkbox>
+    </Box>
+
   )
 }
 
